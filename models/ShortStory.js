6fb6b1b18f506800e195c0b6b925dbc5711c01ed
BrayanAbraham <brayanabraham@gmail.com>
2019-08-13 21:39:55 +0530

    New Models added

---
+++ models/ShortStory.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ShortStorySchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    default: "public"
+  },
+  allowComments: {
+    type: Boolean,
+    default: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  comments: [
+    {
+      commentBody: {
+        type: String,
+        required: true
+      },
+      commentDate: {
+        type: Date,
+        default: Date.now()
+      },
+      commentUserHandle: {
+        type: String,
+        required: true
+      },
+      commentUser: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+module.exports = ShortStory = mongoose.model("shorts", ShortStorySchema);
